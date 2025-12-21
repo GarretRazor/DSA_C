bb8164aa9c8f3fe0c682fb2add88e04721e078f8
William Raezer <wraezer@gmail.com>
2025-12-21 14:43:12 -0500

    Improved traversal and data contents code

--- single_ll.c
+++ single_ll.c
@@ -1,21 +1,30 @@
+#include<stdio.h>
 typedef struct node{
 	int data;
 	struct node* next;
 }node;
-
-
-
-
-
 int main(){
 
-	node node_one;
-	node node_two; 
-        node node_three; 
+	node one;
+	node two; 
+        node three; 
+        one.data = 1;
+        two.data = 2;
+	three.data = 3;
+      	
+	one.next = &node_two;
+	two.next = &node_three;
+	three.next = NULL; 
+        
+	node temp = one;
 
-	node_one.next = &node_two;
-	node_two.next = &node_three;
-	node_three.next = NULL; 
+	while(temp.next != NULL) 
+	{   
+	    int node_count = 1;
+	    printf("Node %d has data %d", node_count, temp.data); 
+	    node_count++; 
+	} 
+	    
 } 
 
 
